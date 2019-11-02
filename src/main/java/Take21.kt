04d649703d2f3a2bef5942e603f8a363f7b93585
ven <venkateshhegde@yahoo.com>
2019-11-02 16:12:05 -0400

    First Kotlin program

---
+++ src/main/java/Take21.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    println("Hello, world!" + args)
+    println("Hello, world!" )
+
+    println("Hello, world!" )
+    println("Hello, world!" )
+    println("Hello, world!" )
+    println("Hello, world!" )
+    println("Hello, world!" )
+    println("Hello, world!" )
+
+
+
+}
+
+fun test()
+{
+    print("hello\n00")
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
